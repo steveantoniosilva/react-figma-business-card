7a8b028f334cb1e98bd9770523d2a0a6e03560f9
Steve Antonio Silva <steveantoniosilva@gmail.com>
2022-07-17 10:51:56 -0400

    removed svg to test deploy

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,4 @@
 import photo from '../assets/business-card-photo.png';
-import linkedin from '../assets/linkedin.png';
 
 export default function Info() {
   return (
@@ -10,9 +9,11 @@ export default function Info() {
         <h2>Fontend Developer</h2>
         <h3>laurasmith.com</h3>
         <section className='buttons'>
-          <button className='left'><span>✉️</span> Email</button>
+          <button className='left'>
+            <span>✉️</span> Email
+          </button>
           <button className='right'>
-            <img className='linkedin' src={linkedin} /> LinkedIn
+            <img src={photo} /> LinkedIn
           </button>
         </section>
       </div>
